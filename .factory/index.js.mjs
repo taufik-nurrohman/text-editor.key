ac2fa9c07fe00c07a4a42709d7d2a6dd096abbd7
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-12-29 20:18:25 +0700

    Update

--- .factory/index.js.mjs
+++ .factory/index.js.mjs
@@ -1,22 +1,60 @@
+import Key from '@taufik-nurrohman/key';
+import {debounce} from '@taufik-nurrohman/tick';
 import {onEvent, offEvent, offEventDefault} from '@taufik-nurrohman/event';
 
-function onKeyDown(e) {
+function onBlur(e) {
+    this.Key.pull(); // Reset all key(s)
+}
 
+function onInput(e) {
+    this.Key.pull(); // Reset all key(s)
+}
+
+function onKeyDown(e) {
+    let command, map = this.Key, v;
+    map.push(e.key); // Add current key to the queue
+    if (command = map.command()) {
+        v = map.fire(command);
+        if (false === v) {
+            offEventDefault(e);
+        } else if (null === v) {
+            console.warn('Unknown command: `' + command + '`');
+        }
+    }
+    this.Bounce();
 }
 
 function onKeyUp(e) {
-
+    this.Key.pull(e.key); // Reset current key
 }
 
 function attach(self) {
     let $ = this;
-    $.commands = $.commands || {};
-    $.keys = $.keys || {};
+    let map = new Key($);
+    $.command = function (command, of) {
+        return ($.commands[command] = of), $;
+    };
+    $.key = function (key, of) {
+        return ($.keys[key] = of), $;
+    };
+    $.commands = map.commands;
+    $.keys = map.keys;
+    onEvent('blur', self, onBlur);
+    onEvent('input', self, onInput);
     onEvent('keydown', self, onKeyDown);
     onEvent('keyup', self, onKeyUp);
+    self.Bounce = debounce(() => map.pull(), 1000); // Reset all key(s) after 1 second idle
+    self.Key = map;
 }
 
 function detach(self) {
+    let $ = this;
+    delete $.commands;
+    delete $.keys;
+    delete self.Bounce;
+    delete self.Key;
+    offEvent('blur', self, onBlur);
+    offEvent('input', self, onInput);
     offEvent('keydown', self, onKeyDown);
     offEvent('keyup', self, onKeyUp);
 }
--- index.js
+++ index.js
@@ -27,8 +27,92 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = f() : typeof define === 'function' && define.amd ? define(f) : (g = typeof globalThis !== 'undefined' ? globalThis : g || self, (g.TextEditor = g.TextEditor || {}, g.TextEditor.Key = f()));
 })(this, (function () {
     'use strict';
+    var isArray = function isArray(x) {
+        return Array.isArray(x);
+    };
+    var isDefined = function isDefined(x) {
+        return 'undefined' !== typeof x;
+    };
+    var isFunction = function isFunction(x) {
+        return 'function' === typeof x;
+    };
+    var isNull = function isNull(x) {
+        return null === x;
+    };
+    var isSet = function isSet(x) {
+        return isDefined(x) && !isNull(x);
+    };
+    var isString = function isString(x) {
+        return 'string' === typeof x;
+    };
+    var toObjectKeys = function toObjectKeys(x) {
+        return Object.keys(x);
+    };
+
+    function Key(self) {
+        var $ = this;
+        var queue = {};
+        $.command = function (v) {
+            if (isString(v)) {
+                return v === $.toString();
+            }
+            var command = $.keys[$.toString()];
+            return isSet(command) ? command : false;
+        };
+        $.commands = {};
+        $.fire = function (command) {
+            var self = $.self || $,
+                value,
+                exist;
+            if (isFunction(command)) {
+                value = command.call(self);
+                exist = true;
+            } else if (isString(command) && (command = $.commands[command])) {
+                value = command.call(self);
+                exist = true;
+            } else if (isArray(command)) {
+                var data = command[1] || [];
+                if (command = $.commands[command[0]]) {
+                    value = command.apply(self, data);
+                    exist = true;
+                }
+            }
+            return exist ? isSet(value) ? value : true : null;
+        };
+        $.key = null;
+        $.keys = {};
+        $.pull = function (key) {
+            $.key = null;
+            if (!isSet(key)) {
+                return queue = {}, $;
+            }
+            return delete queue[key], $;
+        };
+        $.push = function (key) {
+            return queue[$.key = key] = 1, $;
+        };
+        $.self = self;
+        $.toString = function () {
+            return toObjectKeys(queue).join('-');
+        };
+        return $;
+    }
+    var debounce = function debounce(then, time) {
+        var timer;
+        return function () {
+            var _arguments = arguments,
+                _this = this;
+            timer && clearTimeout(timer);
+            timer = setTimeout(function () {
+                return then.apply(_this, _arguments);
+            }, time);
+        };
+    };
     var offEvent = function offEvent(name, node, then) {
         node.removeEventListener(name, then);
+    };
+    var offEventDefault = function offEventDefault(e) {
+        return e && e.preventDefault();
     };
     var onEvent = function onEvent(name, node, then, options) {
         if (options === void 0) {
@@ -37,19 +121,63 @@
         node.addEventListener(name, then, options);
     };
 
-    function onKeyDown(e) {}
+    function onBlur(e) {
+        this.Key.pull(); // Reset all key(s)
+    }
 
-    function onKeyUp(e) {}
+    function onInput(e) {
+        this.Key.pull(); // Reset all key(s)
+    }
+
+    function onKeyDown(e) {
+        var command,
+            map = this.Key,
+            v;
+        map.push(e.key); // Add current key to the queue
+        if (command = map.command()) {
+            v = map.fire(command);
+            if (false === v) {
+                offEventDefault(e);
+            } else if (null === v) {
+                console.warn('Unknown command: `' + command + '`');
+            }
+        }
+        this.Bounce();
+    }
+
+    function onKeyUp(e) {
+        this.Key.pull(e.key); // Reset current key
+    }
 
     function attach(self) {
         var $ = this;
-        $.commands = $.commands || {};
-        $.keys = $.keys || {};
+        var map = new Key($);
+        $.command = function (command, of) {
+            return $.commands[command] = of, $;
+        };
+        $.key = function (key, of) {
+            return $.keys[key] = of, $;
+        };
+        $.commands = map.commands;
+        $.keys = map.keys;
+        onEvent('blur', self, onBlur);
+        onEvent('input', self, onInput);
         onEvent('keydown', self, onKeyDown);
         onEvent('keyup', self, onKeyUp);
+        self.Bounce = debounce(function () {
+            return map.pull();
+        }, 1000); // Reset all key(s) after 1 second idle
+        self.Key = map;
     }
 
     function detach(self) {
+        var $ = this;
+        delete $.commands;
+        delete $.keys;
+        delete self.Bounce;
+        delete self.Key;
+        offEvent('blur', self, onBlur);
+        offEvent('input', self, onInput);
         offEvent('keydown', self, onKeyDown);
         offEvent('keyup', self, onKeyUp);
     }
